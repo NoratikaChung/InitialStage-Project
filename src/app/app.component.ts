a85a90b5c4eaa3b8f4ced1e5c2df5cda0aa96942
NoratikaChung <cnoratika@gmail.com>
2024-06-04 04:35:40 +0800

    Added project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutorial';
+
+  selectedSite: any;
+
+  onSiteSelected(site: any): void {
+    this.selectedSite = site;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { ProjectsComponent} from './components/projects/projects.component';
+// import { FeedbackComponent } from './feedback/feedback.component';
+// import { FormsModule } from '@angular/forms';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ProjectsComponent,
+//     FeedbackComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { SiteDataService } from './site-data.service';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MainDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SiteDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
