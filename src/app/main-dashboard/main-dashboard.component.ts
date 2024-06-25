74493f8bc613a3edaf63f1bfc476440ee85840b8
NoratikaChung <cnoratika@gmail.com>
2024-06-26 00:53:07 +0800

    added description button

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -5,9 +5,30 @@ import { Component, Input } from '@angular/core';
   templateUrl: './main-dashboard.component.html',
   styleUrls: ['./main-dashboard.component.css']
 })
+
 export class MainDashboardComponent {
   @Input() selectedSite: any;
   toolStatus: { [key: string]: boolean } = {};
+
+  descriptionDropdownOpen = false;
+  descriptionData = [
+    { col1: 'Data 1', col2: 'Data A' },
+    { col1: 'Data 2', col2: 'Data B' },
+    { col1: 'Data 3', col2: 'Data C' },
+    { col1: 'Data 4', col2: 'Data D' },
+    { col1: 'Data 5', col2: 'Data E' },
+    { col1: 'Data 6', col2: 'Data F' },
+    { col1: 'Data 7', col2: 'Data G' },
+    { col1: 'Data 8', col2: 'Data H' },
+    { col1: 'Data 9', col2: 'Data I' },
+    { col1: 'Data 10', col2: 'Data J' },
+    { col1: 'Data 11', col2: 'Data K' },
+    { col1: 'Data 12', col2: 'Data L' }
+  ];
+
+  toggleDescriptionDropdown(): void {
+    this.descriptionDropdownOpen = !this.descriptionDropdownOpen;
+  }
 
   toggleLine(line: any): void {
     line.isOpen = !line.isOpen;
